be4b9e31d78bbf4f792e763df3dcf1128773fd90
vuhai-KN <73330788+vuhai-KN@users.noreply.github.com>
2021-01-25 10:16:53 +0700

    Add files via upload

---
+++ 12-amazingRaceV2/src/data/Cat.java
@@ -0,0 +1,23 @@
+package data;
+
+import java.util.Random;
+
+public class Cat extends Pet{
+    //khong co j khac thi thoi, co j rieng thi them vao day, muon do lai ham nao thi viet vao day
+    public static final double MAX_SPEED = 60.0;
+
+    public Cat (String name, int yob, double weight) {
+        super(name, yob, weight);
+    }
+
+    @Override
+    public double run() {
+        return new Random().nextDouble()*MAX_SPEED;
+    }
+
+    @Override
+    public void showRecord() {
+        System.out.printf("CAT    |%-10s|%4d|%6.2f|%6.2f\n", name, yob, weight, run());
+    }
+    
+}
